533acb5fd6e7b28f32a74b1d1e94c5f9a4c093b5
Joshua Granick <jgranick@users.noreply.github.com>
2017-07-05 17:08:29 -0700

    Initial support for export API

--- src/vshaxe/Main.hx
+++ src/vshaxe/Main.hx
@@ -4,9 +4,12 @@ import Vscode.*;
 import vscode.*;
 
 class Main {
-    function new(context:ExtensionContext) {
+    public static var instance:Main;
+    public var server:LanguageServer;
+
+    function new(context:ExtensionContext, ?onReadyCallback) {
         new InitProject(context);
-        var server = new LanguageServer(context);
+        server = new LanguageServer(context, onReadyCallback);
         new Commands(context, server);
 
         setLanguageConfiguration();
@@ -22,6 +25,38 @@ class Main {
     @:keep
     @:expose("activate")
     static function main(context:ExtensionContext) {
-        new Main(context);
+        var api = new Api();
+        init(context, api);
+        return api;
+    }
+
+    static function init(context:ExtensionContext, api:Api) {
+        instance = new Main(context, api.onReadyCallback);
+    }
+}
+
+@:keep class Api {
+    public var isReady(get, null):Bool;
+
+    public function new() {}
+
+    public dynamic function onReady():Void {} // TODO: Support multiple listeners?
+
+    public function onReadyCallback():Void {
+        onReady();
+    }
+
+    public function updateDisplayArguments(args:Array<String>):Void {
+        if (Main.instance != null && isReady) {
+            Main.instance.server.updateDisplayArguments(args);
+        }
+    }
+
+    function get_isReady():Bool {
+        if (Main.instance != null) {
+            return Main.instance.server.isReady;
+        } else {
+            return false;
+        }
     }
 }
